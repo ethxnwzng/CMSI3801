2fd02e3253f1c172721245f3fc16a646142c5174
nickleeds224 <nickoleeds@gmail.com>
2025-09-19 13:03:53 -0700

    fixed the number generator and word adder

--- code/javascript/generator.js
+++ code/javascript/generator.js
@@ -1,7 +1,17 @@
-function* powerGenerator(b, limit) {
-    let power = 1; // b^0 = 1
-    while (power <= limit) {
-      yield power;
-      power *= b;
+function* powerGenerator(b, expCount) {
+    let power = 1;                 // b^0
+    for (let i = 0; i < expCount; i++) {
+      yield power;                 // b^i
+      power *= b;                  // next: b^(i+1)
     }
   }
+  
+  const [b, n] = process.argv.slice(2).map(Number);
+  
+  let sum = 0;
+  for (const p of powerGenerator(b, n)) sum += p;
+  
+  console.log(sum);
+
+
+// node generator.js {number} {limit for summing power}
